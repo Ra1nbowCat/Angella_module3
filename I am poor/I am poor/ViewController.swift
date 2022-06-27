0f961f885e8bd76c7c2aac2631b24b56e3d700ee
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-06-27 19:00:51 +0300

    Completed project for module 3

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Илья Лехов on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
